2f9b07c48caf966dd7d8b80d5f67a216e0f432cd
A-Hussien2011 <a7med.7ussien2011@gmail.com>
2019-10-07 21:13:00 +0200

    front merged with backend

--- laravel-blog/resources/js/components/Example.js
+++ laravel-blog/resources/js/components/Example.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Login from './Login.js';
+import Blog from './Blog.js'
 
 export default class Example extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">I'm an example component!</div>
-                        </div>
-                    </div>
-                </div>
+                <Login/>
             </div>
         );
     }
